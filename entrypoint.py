a485bad7d28c9d0c2d25ca91a2af21f1bd5c8144
Alexey Ryabtsev <alexey.ryabtsev@cybercradle.com>
2023-04-11 13:31:01 +0100

    version1

--- entrypoint.py
+++ entrypoint.py
@@ -4,35 +4,37 @@ import boto3
 import os,re
 import glob
 from botocore.config import Config
-from dotenv import load_dotenv
 
 os.chdir(os.environ['WORKING_PATH'])
 load_dotenv()
 
 def upload_with_content_type_gzip(file, ext):
-    client.upload_file(file, os.getenv('AWS_BUCKET'), file, ExtraArgs={'ContentType': ext, 'ContentEncoding': 'gzip', 'ACL': 'public-read'})
+    client.upload_file(file, os.environ['AWS_BUCKET'], file, ExtraArgs={'ContentType': ext, 'ContentEncoding': 'gzip', 'ACL': 'public-read'})
     print(file, ext)
 
 def upload_with_content_type(file, ext):
-    client.upload_file(file, os.getenv('AWS_BUCKET'), file, ExtraArgs={'ContentType': ext, 'ACL': 'public-read'})
+    client.upload_file(file, os.environ['AWS_BUCKET'], file, ExtraArgs={'ContentType': ext, 'ACL': 'public-read'})
     print(file, ext)
 
 client = boto3.client('s3', 
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'), 
-    aws_secret_access_key=os.getenv('AWS_SECRET_KEY'), 
-    region_name=os.getenv('AWS_REGION')
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY'], 
+    aws_secret_access_key=os.environ['AWS_SECRET_KEY'], 
+    region_name=os.environ['AWS_REGION']
 )
 
 
 file_types = {
+'.bin': 'binary/octet-stream',
 '.css': 'text/css',
+'.gltf': 'binary/octet-stream',
+'.gz': 'application/x-gzip',
 '.html': 'text/html',
 '.jpeg': 'image/jpeg',
 '.jpg': 'image/jpeg',
 '.js': 'text/javascript',
 '.png': 'image/png',
 '.svg': 'image/svg+xml',
-'.txt': 'text/plain',
+'.txt': 'text/plain'
 }
 
 file_list = glob.glob('**/*.*', recursive=True)
