6748c51924611b99807cedd475978720b40c0986
Aliaksiej Rabcau <61757573+chicagomg@users.noreply.github.com>
2023-04-27 22:04:14 +0100

    update

--- entrypoint.py
+++ entrypoint.py
@@ -5,18 +5,28 @@ import os,re
 import glob
 from botocore.config import Config
 
-print("plugin v1.4.3")
+print("plugin v1.5.0")
 os.chdir(os.environ['WORKING_PATH'])
+
+file_types = {
+'.bin': 'binary/octet-stream',
+'.css': 'text/css',
+'.gltf': 'model/gltf+json',
+'.gz': 'application/x-gzip',
+'.hdr': 'binary/octet-stream',
+'.html': 'text/html',
+'.jpeg': 'image/jpeg',
+'.jpg': 'image/jpeg',
+'.js': 'text/javascript',
+'.png': 'image/png',
+'.svg': 'image/svg+xml',
+'.txt': 'text/plain',
+'.webp': 'binary/octet-stream'
+}
 
 def upload_with_content_type_gzip(file):
     new_file = file[:-3]
-    new_file_types = {
-    '.bin': 'binary/octet-stream',
-    '.css': 'text/css',
-    '.gltf': 'binary/octet-stream',
-    '.hdr': 'binary/octet-stream',
-    '.js': 'text/javascript',    
-    }
+
     for i in new_file_types.keys():
         my_regex = r".*" + re.escape(i) + r"$"
         for m in re.findall(my_regex, new_file, re.IGNORECASE):
@@ -37,24 +47,18 @@ client = boto3.client('s3',
     region_name=os.environ['AWS_REGION']
 )
 
-file_types = {
-'.gz': 'application/x-gzip',
-'.html': 'text/html',
-'.jpeg': 'image/jpeg',
-'.jpg': 'image/jpeg',
-'.png': 'image/png',
-'.svg': 'image/svg+xml',
-'.txt': 'text/plain'
-}
+
 
 file_list = glob.glob('**/*.*', recursive=True)
+
 
 for file in file_list:
     for i in file_types.keys():
         my_regex = r".*" + re.escape(i) + r"$"
         for m in re.findall(my_regex, file, re.IGNORECASE):
             ext = file_types.get(i)
-            if ext == 'application/x-gzip':
-                upload_with_content_type_gzip(file)
-            else:
+            if ext != 'application/x-gzip':
                 upload_with_content_type(file, ext)
+            else:
+                if os.environ['CDN_COMPRESSION'] == True:
+                    upload_with_content_type_gzip(file)
